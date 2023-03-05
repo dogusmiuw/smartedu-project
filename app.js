a1c7e88d5baa9e703d71de0472c0f024f59da619
Dogukan Celik <dogukancelikcse@gmail.com>
2023-03-05 22:56:48 +0300

    header, navigation, footer templates created.

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// TEMPLATE ENGINE
+app.set("view engine","ejs");
 
-app.get("/", (req,res)=>{
-    res.send("<h1>index</h1>");
+// MIDDLEWARES
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.render("index", {
+        current_page:"index"
+    });
 });
+
+app.get("/about", (req, res)=>{
+    res.render("about", {
+        current_page: "about"
+    });
+});
+
 
 
 // END
